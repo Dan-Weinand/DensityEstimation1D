dd65f6e810b20a315f19020ee710ae9b3d9f9ed0
Gedeon Nyengele <gedeonnyengele@hotmail.com>
2014-06-03 17:52:26 -0400

    Created the SettingUI class

--- WaveletFiles/SettingsUI.java
+++ WaveletFiles/SettingsUI.java
@@ -1,3 +1,4 @@
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
 
 /**
@@ -7,4 +8,22 @@ import javax.swing.JFrame;
  */
 public class SettingsUI extends JFrame{
 
+	public SettingsUI(int width, int height){
+		
+		this.setSize(width, height);
+		init();
+		
+		
+	}
+	public SettingsUI()
+	{
+		init();
+	}
+	
+	public void init()
+	{
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout( new FlowLayout() );
+	}
 }
